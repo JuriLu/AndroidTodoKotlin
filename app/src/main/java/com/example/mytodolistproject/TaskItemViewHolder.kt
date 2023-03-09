cb9a96024281a644410113eb453eb3246fceec59
Juri <lubonjajurgen1@gmail.com>
2023-03-09 14:01:20 +0100

    Task Item View Holder

---
+++ app/src/main/java/com/example/mytodolistproject/TaskItemViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.mytodolistproject
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mytodolistproject.databinding.TaskItemCellBinding
+
+class TaskItemViewHolder(
+    private val context: Context,
+    private val binding: TaskItemCellBinding,
+//    private val clickListener: TaskItemClickListener
+): RecyclerView.ViewHolder(binding.root){
+
+}
