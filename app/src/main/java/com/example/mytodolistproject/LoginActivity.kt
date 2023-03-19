9df1221f2f5b3261ffa34e20184508ad01e53a67
Juri <lubonjajurgen1@gmail.com>
2023-03-19 17:05:13 +0100

    Login finished

--- app/src/main/java/com/example/mytodolistproject/LoginActivity.kt
+++ app/src/main/java/com/example/mytodolistproject/LoginActivity.kt
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+       val username = findViewById<TextView>(R.id.username)
+       val password = findViewById<TextView>(R.id.password)
+
 
 
         findViewById<Button>(R.id.loginbtn)
             .setOnClickListener {
-                Log.d("BUTTONS", "User tapped the Supabutton")
-                val login = Intent(this, MainActivity::class.java)
-                startActivity(login)
+                if (username.text.toString().equals("admin") && password.text.toString().equals("admin")){
+                    Log.d("Login Btn", "LOGIN SUCCESSFUL")
+                    val mainAct = Intent(this, MainActivity::class.java)
+                    startActivity(mainAct)
+                } else {
+                    Toast.makeText(getBaseContext(),"Login not successful",Toast.LENGTH_LONG).show();
+                }
+
             }
     }
 }
